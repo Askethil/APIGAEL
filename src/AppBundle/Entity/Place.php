ab83249bafb7ea0208db833df24481db59d04577
Askethil <g.leboedec@hotmail.fr>
2017-01-04 10:56:02 +0100

    mise à jour de test pour les  select par id et all de l'api

---
+++ src/AppBundle/Entity/Place.php
@@ -0,0 +1,71 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="places")
+ */
+
+class Place
+{
+      /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+    /**
+     * @ORM\Column(type="string")
+     */
+    public $name;
+    /**
+     * @ORM\Column(type="string")
+     */
+    public $address;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function setAddress($address)
+    {
+        $this->address = $address;
+        return $this;
+    }
+    public function __construct($name, $address)
+    {
+        $this->name = $name;
+        $this->address = $address;
+    }
+}
